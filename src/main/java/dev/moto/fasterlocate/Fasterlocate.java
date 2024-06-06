18c7f779f42ee7353d46def597337535fe18a330
Shoshak <shoshak@github.com>
2024-06-06 18:35:48 +0500

    Initial commit

---
+++ src/main/java/dev/moto/fasterlocate/Fasterlocate.java
@@ -0,0 +1,57 @@
+package dev.moto.fasterlocate;
+
+import com.mojang.logging.LogUtils;
+import net.minecraft.core.BlockPos;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import org.slf4j.Logger;
+
+import java.io.*;
+import java.util.Optional;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(Fasterlocate.MODID)
+public class Fasterlocate {
+    // Define mod id in a common place for everything to reference
+    public static final String MODID = "fasterlocate";
+    // Directly reference a slf4j logger
+    private static final Logger LOGGER = LogUtils.getLogger();
+    private static long SEED;
+
+    public Fasterlocate() {
+        // Register ourselves for server and other game events we are interested in
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    public static Optional<BlockPos> getTreasureLocation(BlockPos startingPos) {
+        Runtime r = Runtime.getRuntime();
+        try {
+            Process p = r.exec(String.format("./cubiomes_generator %s %s %s", SEED, startingPos.getX(), startingPos.getZ()));
+            p.waitFor();
+            BufferedReader b = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            BufferedReader errors = new BufferedReader(new InputStreamReader(p.getErrorStream()));
+            String err = errors.readLine();
+            if (err != null && !err.isEmpty()) {
+                System.out.println(err);
+                return Optional.empty();
+            }
+            String answer = b.readLine();
+            String[] xz = answer.split(" ");
+            return Optional.of(new BlockPos(Integer.parseInt(xz[0]), 64, Integer.parseInt(xz[1])));
+        } catch (IOException e) {
+            LOGGER.error("Could not read a.out");
+            return Optional.empty();
+        } catch (InterruptedException e) {
+            LOGGER.error("Interrupted a.out");
+            return Optional.empty();
+        }
+    }
+
+    // You can use SubscribeEvent and let the Event Bus discover methods to call
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event) {
+        SEED = event.getServer().overworld().getSeed();
+    }
+}
---
+++ src/main/java/dev/moto/fasterlocate/mixin/ChunkGeneratorMixin.java
@@ -0,0 +1,26 @@
+package dev.moto.fasterlocate.mixin;
+
+import dev.moto.fasterlocate.Fasterlocate;
+import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.tags.StructureTags;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.level.levelgen.structure.Structure;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.Optional;
+
+@Mixin(ServerLevel.class)
+public abstract class ChunkGeneratorMixin {
+    @Inject(at = @At("HEAD"), method = "findNearestMapStructure", cancellable = true)
+    private void locateButFaster(TagKey<Structure> structure, BlockPos pos, int p_215014_, boolean p_215015_, CallbackInfoReturnable<BlockPos> cir) {
+        if (pos.getX() < 50000 && pos.getZ() < 50000 && pos.getX() > -50000 && pos.getZ() > -50000
+                && structure.equals(StructureTags.ON_TREASURE_MAPS)) {
+            Optional<BlockPos> foundPosition = Fasterlocate.getTreasureLocation(pos);
+            foundPosition.ifPresent(cir::setReturnValue);
+        }
+    }
+}
